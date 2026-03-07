693f42156f73e91a846cef8b32b704cb68e4dd15
Sushant15 <sushantgarg15@gmail.com>
2026-03-07 20:08:22 +0530

    Update wsgi.py
    Capitalize api to API in row2

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from a2wsgi import ASGIMiddleware
-from api.main import app  # main.py where FastAPI() is defined
+from API.main import app  # main.py where FastAPI() is defined
 
 application = ASGIMiddleware(app)
